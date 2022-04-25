89ad5e2c5169da3b097965391769a8098d017a9a
PedroHoSantos <pedrohenrique.osantos@hotmail.com>
2022-04-25 16:43:23 -0300

    teste

---
+++ 25.04/carros/Carro.java
@@ -0,0 +1,7 @@
+package carros;
+class Carro extends Veiculo {
+    public static void main(String[] args) {
+        Carro meuCarro = new Carro();
+        meuCarro.roncoMotor();
+    }
+}
---
+++ 25.04/carros/Veiculo.java
@@ -0,0 +1,6 @@
+package carros;
+class Veiculo {
+    public void roncoMotor() {
+        System.out.println("Tuut, tuut!");
+    }
+}
